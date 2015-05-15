2704c21f54315386255b3fe8076b7ddeb214ea1d
Mike Luby <mLuby@users.noreply.github.com>
2015-05-15 00:16:17 -0700

    initial proof of concept

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+[ ] v0 establish connection with a peer
+[ ] v0 share an update with a peer
+[ ] v0 receive an update from a peer
+[ ] v0 change DOM element based on update from peer
+[ ] v1 verify update from a peer is valid
+[ ] v1 request additional peers connection with a peer
+ */
+
