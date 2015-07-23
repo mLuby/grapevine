9050c0d0a94becd89d51e99b7d75224cbb5ed3bb
Kevin Chen <kevin.chen@indiegogo.com>
2015-07-23 15:17:13 -0700

    refactored into grapevine-client

--- client/app.js
+++ client/app.js
@@ -1,14 +1,17 @@
-var app = angular.module('appThatUsesGrapevine', []);
-
-app.controller('aController', ['Grapevine', '$scope', function aController(aService, $scope){
-  this.gv = aService;
-  this.toHumanDateTime = function(t){ return (new Date(t)).toLocaleTimeString(); };
-}]);
-
-app.service('aService', ['$http', '$rootScope', function aService($http, $rootScope){
-  var context = this;
-  context.data = {
-    messages: [],
-  };
-  return context.data;
-}]);
+// var app = angular.module('appThatUsesGrapevine', []);
+//
+// app.controller('aController', ['Grapevine', '$scope', function aController(aService, $scope){
+//   this.gv = aService;
+//   this.toHumanDateTime = function(t){ return (new Date(t)).toLocaleTimeString(); };
+// }]);
+//
+// app.service('aService', ['$http', '$rootScope', function aService($http, $rootScope){
+//   var context = this;
+//   context.data = {
+//     messages: [],
+//   };
+//   return context.data;
+// }]);
+var f = new Grapevine();
+f.connect({host: 'localhost', port: 3000, path: '/webrtc'});
+console.log(f);
--- client/grapevine-client.js
+++ client/grapevine-client.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Grapevine = (function() {
+var Grapevine = (function () {
   var id = ''
   var children = [];
   var parents = [];
@@ -11,26 +11,24 @@ var Grapevine = (function() {
     id = createUniqueId({ length:19 });
   }
 
-  Grapevine.prototype.connect = function(url) {
+  Grapevine.prototype.connect = function(options) {
     var thisPeer = new Peer(id, options);
+    var url = 'http://' + options.host + ':' + options.port;
 
     // The server connects
-    thisPeer.on('open', function(id){
-      // TODO generic GET; not Angular
-      $http.get('http://localhost:3000/children').success(function(childrenIds){
-        console.log('children', childrenIds);
-        childrenIds.forEach(function(childId){
+    thisPeer.on('open', function(id) {
+      makeRequest(url + '/children', function(childIds) {
+        childIds.forEach(function(childId) {
           var peerDataConnection = thisPeer.connect(childId);
-          handleOpenConnection(peerDataConnection, {isChild: true});
+          handlePeerConnection(thisPeer, peerDataConnection, { isChild: true });
         });
       });
-    })
+    });
 
     thisPeer.on('server-message', processMessage);
 
-    // A peer connects
     thisPeer.on('connection', function(peerDataConnection) {
-      handlePeerConnection(peerDataConnection, { isChild:false });
+      handlePeerConnection(thisPeer, peerDataConnection, { isChild:false });
     })
 
     thisPeer.on('disconnected', function() {
@@ -46,11 +44,13 @@ var Grapevine = (function() {
     });
   }
 
-  function handlePeerConnection(peerDataConnection, options){
+  function handlePeerConnection(thisPeer, peerDataConnection, options) {
     // A peer connects
     peerDataConnection.on('open', function() {
       var parentsOrChildren = options.isChild ? children : parents;
       parentsOrChildren.push({ id: peerDataConnection.peer, peerDataConnection:peerDataConnection, status:'connected' });
+      console.log('children: ', children);
+      console.log('parents: ', parents);
       // Auto-disconnect when this peer has enough parents.
       if(parents.length >= expectedNumParents){
         thisPeer.disconnect();
@@ -61,8 +61,8 @@ var Grapevine = (function() {
     peerDataConnection.on('data', processMessage);
 
     // A peer closes connection to this peer
-    peerDataConnection.on('close', function(){ 
-      whenAPeerClosesConnection(peerDataConnection.peer, parents, thisPeer); 
+    peerDataConnection.on('close', function(){
+      whenAPeerClosesConnection(peerDataConnection.peer, parents, thisPeer);
     });
 
     peerDataConnection.on('error', function(err) {
@@ -70,60 +70,95 @@ var Grapevine = (function() {
     });
   }
 
-  return Grapevine;;
+  function createUniqueId(options) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for( var i=0; i < ((options && options.length) || 12); i++ ){
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+
+
+  function whenAPeerClosesConnection(peerId, thisPeer, parents, children) {
+    findPeerByID(peerId, parents, children).status = 'closed';
+    var allParentsAreClosed = parents.reduce(function(areAllClosed, parent) { return areAllClosed && parent.status === 'closed' ? true : false; }, true);
+    if(allParentsAreClosed) {
+      console.log('I`ve become an orphan. Reconnecting...');
+      thisPeer.reconnect();
+    }
+  }
+
+  function findPeerByID(id, parents, children) {
+    // TODO: refactor
+    console.log(parents);
+    console.log(children);
+    var parentsIndex = -1, childrenIndex = -1;
+    if (parents) {
+      parentsIndex = parents.map(function(peer) { return peer.id; }).indexOf(id);
+    }
+    if (children) {
+      childrenIndex = children.map(function(peer) { return peer.id; }).indexOf(id);
+    }
+
+    return ~parentsIndex ? parents[parentsIndex] : children[childrenIndex];
+  }
+
+  function processMessage(message) {
+    // TODO interact with user-supplied callback
+    if(!alreadyReceived(message) && verifiedFromServer(message)) {
+      messages.push(message);
+      console.log('received', message);
+      sendToAllConnections(message);
+    }
+
+    function alreadyReceived(message){
+      var messageIndex = messages.map(function(message){ return message.timestamp; }).indexOf(message.timestamp);
+      return ~messageIndex ? true: false;
+    }
+    function verifiedFromServer(message, serverPublicKey){
+      // TODO add message verification/decryption
+      return true;
+    }
+  }
+
+  function sendToAllConnections(message){
+    var temp = message.sender; // TODO -_-
+    message.sender = id
+    children.forEach(function(child){
+      sendToConnection(child.id, message);
+    });
+    message.sender = temp;
+  }
+
+  function sendToConnection(peerId, data){
+    findPeerByID(peerId, parents, children).peerDataConnection.send(data);
+  }
+
+  function makeRequest(url, callback) {
+    var httpRequest;
+    if (window.XMLHttpRequest) {
+      httpRequest = new XMLHttpRequest();
+    }
+    if (!httpRequest) {
+      // TODO: handle this error
+      return false;
+    }
+    httpRequest.onreadystatechange = responseHandler; // res
+    httpRequest.open('GET', url);
+    httpRequest.send();
+
+    function responseHandler() {
+      if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+          // TODO: parse / deal with responseText
+          callback(JSON.parse(httpRequest.responseText));
+        } else {
+          alert('There was a problem with the request.');
+        }
+      }
+    }
+  }
+
+  return Grapevine;
 })();
-
-function createUniqueId(options) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for( var i=0; i < options.length || 12; i++ ){
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
-
-function whenAPeerClosesConnection(peerId, thisPeer, parents, children) {
-  findPeerByID(peerId, parents, children).status = 'closed';
-  var allParentsAreClosed = parents.reduce(function(areAllClosed, parent){ return areAllClosed && parent.status === 'closed' ? true : false; }, true);
-  if(allParentsAreClosed) {
-    console.log('I`ve become an orphan. Reconnecting...');
-    thisPeer.reconnect();
-  }
-}
-
-function findPeerByID(id, parents, children){
-  var parentsIndex = parents.map(function(peer){ return peer.id; }).indexOf(id);
-  var childrenIndex = children.map(function(peer){ return peer.id; }).indexOf(id);
-  return ~parentsIndex ? parents[parentsIndex] : children[childrenIndex];
-}
-
-function processMessage(message) {
-  // TODO interact with user-supplied callback
-  if(!alreadyReceived(message) && verifiedFromServer(message)) {
-    messages.push(message);
-    console.log('received', message);
-    sendToAllConnections(message);
-  }
-
-  function alreadyReceived(message){
-    var messageIndex = self.data.messages.map(function(message){ return message.timestamp; }).indexOf(message.timestamp);
-    return ~messageIndex ? true: false;
-  }
-  function verifiedFromServer(message, serverPublicKey){
-    // TODO add message verification/decryption
-    return true;
-  }
-}
-
-function sendToAllConnections(message){
-  var temp = message.sender; // TODO -_-
-  message.sender = id
-  children.forEach(function(child){
-    sendToConnection(child.id, message);
-  });
-  message.sender = temp;
-}
-
-function sendToConnection(peerId, data){
-  findPeerByID(peerId).dataConnection.send(data);
-}
