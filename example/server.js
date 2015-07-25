a6026a53d53d3c8b8a121a2c512162e0a147e802
Mike Luby <mLuby@users.noreply.github.com>
2015-07-25 16:33:14 -0700

    fix example

--- example/server.js
+++ example/server.js
@@ -3,7 +3,6 @@ var app = express();
 var server = app.listen(3000, function(){
   console.log('Server listening at localhost:3000');
 });
-var path = require('path');
 
 var Grapevine = require('grapevine-server');
 Grapevine.setup('/webrtc', app, server);
@@ -13,6 +12,4 @@ app.get('/sendMessage', function(req, res){
   return res.sendStatus(200);
 });
 
-app.get('/', function(req, res){
-  return res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use('/', express.static('client'));
